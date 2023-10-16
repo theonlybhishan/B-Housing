089eee41c6e0a85eff71befd5e89917bf4c05f60
theonlybhishan <bhishanpoudel17@gmail.com>
2023-10-16 21:18:09 +0545

    changed

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python:3.9-slim
-WORKDIR /app
-COPY ./requirements.txt /app/
-RUN pip install -r requirements.txt
-COPY . /app/
--- app/app.py
+++ app/app.py
@@ -34,4 +34,5 @@ def predict():
 
 
 if __name__=="__main__":
-    app.run(debug=True, port=5000, host="0.0.0.0")
+    app.run(debug=True)
+    # app.run(debug=True, port=5000, host="0.0.0.0")
